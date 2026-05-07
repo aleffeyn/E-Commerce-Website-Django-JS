0eb44fda3db3ff9ca75312d1e8f92319d9770b08
Alefeyn <hossein.sh.shahamat@gmail.com>
2026-05-07 11:20:26 +0200

    Fixxing an Url Issue which were preventing the client to add comment

--- article_module/urls.py
+++ article_module/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ArticleListView.as_view() , name='article_list'),
     path('cat/<str:category>', views.ArticleListView.as_view(), name='article_by_category_filter'),
-    path('add-article-comment/', views.add_article_comment, name='add_article_comment'),
+    path('add-article-comment', views.add_article_comment, name='add_article_comment'),
     path('<pk>' , views.ArticleDetailView.as_view() , name='article_detail'),
 ]
